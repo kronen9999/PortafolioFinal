36042055127cc21edaaed4427822baafe6909e4c
kronen9999 <stacica22eneronkio@gmail.com>
2025-08-18 18:03:20 -0600

    Se han agregado estilos al contenedor padre basico y tambien a la barra de menu

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MenuBar from './components/MenuBar'
+import './index.css'
+
+function App() {
+  return (
+    <>
+     <div className='father_container'>
+      <div className='container_style'>
+      <div className='container_content'>
+      <MenuBar />  
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/MenuBar.css"
+
+function MenuBar() {
+  return (
+    <div className='menu_bar'>
+        
+    </div>
+  )
+}
+
+export default MenuBar
