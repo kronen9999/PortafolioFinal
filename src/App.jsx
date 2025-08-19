0ec73caf1a1d7d214879eb55f8f1d0ca7940dc60
kronen9999 <stacica22eneronkio@gmail.com>
2025-08-19 15:30:20 -0600

    Se ha agregado una estructura base para la seccion de about me

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,70 @@ function App() {
       <div className='container_content'>
       <MenuBar />
       <section className='About_Section' id='About'>
-       
+       <div className='d1'>
+       <p className='p1'>
+        Full Stack Developer
+       </p>
+       <p className='p2'>
+        Passionate about creating innovative web solutions that bridge the gap between beautiful design and robust functionality.
+       </p>
+       <div className='div_Refs'>
+       <a href="#Projects">
+        View proyects
+       </a>
+       <a href="#Contact">Get in touch</a>
+       </div>
+       </div>
+       <div className='d2'>
+        <p className='p1'>
+          About me
+        </p>
+        <p className='p2'>
+         I'm a passionate full stack developer with experience in building scalable web applications. I enjoy working with modern technologies and creating solutions that make a difference.
+        </p>
+        <div className='Tecnologies'>
+          <p className='pt'>
+            Java
+          </p>
+          <p className='pt'>
+            Javascript
+          </p>
+          <p className='pt'>
+            C#
+          </p>
+          <p className='pt'>
+            React
+          </p>
+          <p className='pt'>
+            Html
+          </p>
+          <p className='pt'>
+            Css
+          </p>
+          <p className='pt'>
+            Git
+          </p>
+          <p className='pt'>
+            Postman
+          </p>
+          <p className='pt'>
+            Mysql
+          </p>
+          <p className='pt'>
+            Sql
+          </p>
+          <p className='pt'>
+           Php
+          </p>
+          <p className='pt'>
+            Laravel
+          </p>
+        </div>
+          <hr/>
+          <p className='p3'>
+            📍 Available for freelance projects and full-time opportunities
+          </p>
+       </div>
       </section>  
       <section className='Projects_Section' id='Projects'>
 
